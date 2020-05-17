310a0825f9493481f7a7d614e4cd5a0ee5d4f5f1
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-05-17 21:07:51 +0300

    Updates

---
+++ precache-manifest.0e5931db99e36cfae6653262e0c1c4a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fad84b5b7f9efcccde722be31bc8301",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c8eba2d66b1d6cf8c4a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "99db674b87d7e22804c1",
+    "url": "/static/css/main.f7b37a0f.chunk.css"
+  },
+  {
+    "revision": "3c8eba2d66b1d6cf8c4a",
+    "url": "/static/js/2.71c85b43.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.71c85b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99db674b87d7e22804c1",
+    "url": "/static/js/main.3f65722e.chunk.js"
+  },
+  {
+    "revision": "d68632a5d0e432577bc4",
+    "url": "/static/js/runtime-main.950badf7.js"
+  },
+  {
+    "revision": "06253f1aa2352a3cf0d41431a5b5886d",
+    "url": "/static/media/homephoto.06253f1a.jpg"
+  }
+]);
